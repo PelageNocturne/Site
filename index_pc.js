b6b0a3739bc640741c6d70dbaf20392da6be0d1b
PelageNocturne <91083500+PelageNocturne@users.noreply.github.com>
2022-05-17 19:06:17 +0200

    Create index_pc.js

---
+++ index_pc.js
@@ -0,0 +1,107 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="icon" href="../icon.ico">
+        <title>Lenoria | Accueil</title>
+        <link rel="stylesheet" href="../css/main.css">
+        <script src="https://kit.fontawesome.com/9ab044b390.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="menu">
+            <div class="back">
+                <div class="mobile">
+                    <span></span>
+                </div>
+                <div class="items">
+                    <a href="index.html" class="item">Accueil</a>
+                    <a href="news.html" class="item">News</a>
+                    <a href="reseaux.html" class="item">Réseaux</a>
+                    <a href="boutique.html" class="item">Boutique</a>
+                    <a href="wiki.html" class="item">Wiki</a>
+                </div>
+            </div>
+        </div>
+        <div class="RRC-header-bg"></div>
+        <div class="header">
+            <div class="header-nav">
+                <div class="header-inner container">
+                    <div class="RRC-nav-items">
+                        <div class="logo--1">
+                            <img class="logo" src="../icon.ico" alt="logo" width="50">
+                        </div>
+                        <div class="item--1">
+                            <a href="index.html" class="item active">Accueil</a>
+                            <a href="news.html" class="item">News</a>
+                            <a href="reseaux.html" class="item">Réseaux</a>
+                            <a href="boutique.html" class="item">Boutique</a>
+                            <a href="wiki.html" class="item">Wiki</a>
+                        </div>
+                        <div class="item--2">
+                            <a href="login.html" class="item-connexion"><button>Connexion</button></a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <script>
+
+            if( navigator.userAgent.match(/iPhone/i)
+                || navigator.userAgent.match(/webOS/i)
+                || navigator.userAgent.match(/Android/i)
+                || navigator.userAgent.match(/iPad/i)
+                || navigator.userAgent.match(/iPod/i)
+                || navigator.userAgent.match(/BlackBerry/i)
+                || navigator.userAgent.match(/Windows Phone/i)
+            ){
+    
+                document.write('<code html pour mobile>');
+    
+            } else {
+    
+                document.write('<code html pour ordi>');
+    
+            }
+    
+    </script>
+    
+        <footer>
+            <h5>Des questions ? Allez sur le Discord et ouvrez un ticket</h5>
+            <div class="colonnes">
+                <div class="colonne">
+                    <p><a class="text_footer">À propos</a></p>
+                    <p>Lenoria est un serveur<br>OneBlock crée en 2021<br>avec un gameplay farm2win<br>et divertissant.</p>
+                </div>
+    
+                <div class="colonne">
+                    <p><a class="text_footer">Nous rejoindre</a></p>
+                    <p><a href="" class="item-download">Windows</a></p>
+                    <p><a href="" class="item-download">Mac</a></p>
+                    <p><a href="" class="item-download">Linux</a></p>
+                </div>
+    
+                <div class="colonne">
+                    <p><a class="text_footer">Nous contacter</a</p>
+                    <p><a href="https://instagram.com" target="_blanck" class="lien_footer">Instagram</a></p>
+                    <p><a href="https://discord.gg/yqTafhC6nP" target="_blanck" class="lien_footer">Discord</a></p>
+                    <p><a href="https://www.youtube.com/channel/UCrpAL7vPtljXq3TASe3gfAQ" target="_blanck" class="lien_footer">YouTube</a></p>
+                </div>
+            </div>
+            <p>Lenoria 2022 Tous Droits Réservés ©</p>
+        </footer>    
+    
+    </body>
+</html>
